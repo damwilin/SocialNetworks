ea935ef91a476152a0e16fc0053dfe4a46f2e2d2
damwilin <damian.wilinski@gmail.com>
2017-11-02 16:02:55 +0100

    Create JUnit test for CapGraph class

--- src/graph/CapGraph.java
+++ src/graph/CapGraph.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package graph;
 
@@ -9,58 +9,78 @@ import java.util.List;
 
 /**
  * @author Damian Wilinski.
- * 
- * For the warm up assignment, you must implement your Graph in a class
- * named CapGraph.  Here is the stub file.
- *
+ *         <p>
+ *         For the warm up assignment, you must implement your Graph in a class
+ *         named CapGraph.  Here is the stub file.
  */
 public class CapGraph implements Graph {
 
-	private HashMap<Integer,HashSet<Integer>> graphMap = new HashMap<Integer,HashSet<Integer>>();
-	int graphMapSize = 0;
+    private HashMap<Integer, HashSet<Integer>> graphMap = new HashMap<Integer, HashSet<Integer>>();
+    int graphMapSize = 0;
+    int edgeSize = 0;
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#addVertex(int)
-	 */
-	@Override
-	public void addVertex(int num) {
-		graphMap.put(graphMapSize++, new HashSet<Integer>());
-	}
+    /* (non-Javadoc)
+     * @see graph.Graph#addVertex(int)
+     */
+    @Override
+    public void addVertex(int num) {
+        if (!graphMap.containsKey(num))
+            graphMap.put(num, new HashSet<Integer>());
+        graphMapSize++;
+    }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#addEdge(int, int)
-	 */
-	@Override
-	public void addEdge(int from, int to) {
-		if (!graphMap.containsKey(from) || !graphMap.containsKey(to))
-			throw new IllegalArgumentException("There is no: " + from + " " + to);
-		graphMap.get(from).add(to);
-	}
+    /* (non-Javadoc)
+     * @see graph.Graph#addEdge(int, int)
+     */
+    @Override
+    public void addEdge(int from, int to) {
+        if (!graphMap.containsKey(from) || !graphMap.containsKey(to))
+            throw new IllegalArgumentException("There is no: " + from + " " + to);
+        graphMap.get(from).add(to);
+        edgeSize++;
+    }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#getEgonet(int)
-	 */
-	@Override
-	public Graph getEgonet(int center) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see graph.Graph#getEgonet(int)
+     */
+    @Override
+    public Graph getEgonet(int center) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#getSCCs()
-	 */
-	@Override
-	public List<Graph> getSCCs() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see graph.Graph#getSCCs()
+     */
+    @Override
+    public List<Graph> getSCCs() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#exportGraph()
-	 */
-	@Override
-	public HashMap<Integer, HashSet<Integer>> exportGraph() {
-		return graphMap;
-	}
+    /* (non-Javadoc)
+     * @see graph.Graph#exportGraph()
+     */
+    @Override
+    public HashMap<Integer, HashSet<Integer>> exportGraph() {
+        return graphMap;
+    }
+
+    public int getGraphSize() {
+        return graphMapSize;
+    }
+
+    public boolean isVertex(int vertex) {
+        return graphMap.containsKey(vertex);
+    }
+
+    public boolean isEdge(int from, int to) {
+        if (!graphMap.containsKey(from) || !graphMap.containsKey(to))
+            throw new IllegalArgumentException("There is no vertex: " + from + " " + to);
+        if (graphMap.get(from).contains(to))
+            return true;
+        else
+            return false;
+    }
 
 }
---
+++ src/tests/CapGraphTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import graph.CapGraph;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by Damian on 11/2/2017.
+ */
+public class CapGraphTest {
+    CapGraph graphEmpty;
+    CapGraph graphSmall;
+
+    @Before
+    public void setUp() {
+        graphEmpty = new CapGraph();
+        graphSmall = new CapGraph();
+        graphSmall.addVertex(1);
+        graphSmall.addVertex(2);
+        graphSmall.addEdge(1, 2);
+    }
+
+    @Test
+    public void testSize() {
+        Assert.assertEquals(graphEmpty.getGraphSize(), 0);
+        Assert.assertEquals(graphSmall.getGraphSize(), 2);
+    }
+
+    @Test
+    public void testHasVertex() {
+        Assert.assertFalse(graphEmpty.isVertex(0));
+        Assert.assertFalse(graphSmall.isVertex(0));
+        Assert.assertTrue(graphSmall.isVertex(1));
+        Assert.assertTrue(graphSmall.isVertex(2));
+    }
+
+    @Test
+    public void testHasEdge() {
+        Assert.assertFalse(graphSmall.isEdge(2, 1));
+        Assert.assertTrue(graphSmall.isEdge(1, 2));
+    }
+
+    @Test
+    public void testAddEdge() {
+        try {
+            graphEmpty.addEdge(1, 2);
+            fail("There is no 1,2");
+        } catch (IllegalArgumentException e) {
+        }
+
+    }
+}
